9813c5e580fc8f648765900b829b259a8268b57e
GML <a.chen.guang>
2018-12-31 18:21:39 +0800

    Initial Commit

---
+++ GMLRenameFile/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GMLRenameFile
+//
+//  Created by GML on 2018/12/31.
+//  Copyright © 2018 GML. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GMLRenameFile/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GMLRenameFile
+//
+//  Created by GML on 2018/12/31.
+//  Copyright © 2018 GML. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
